4abce44522f9fb7da5607f14a08dba67dbf1e57b
Daniel Engbert <end1@umbc.edu>
2016-10-02 03:57:56 -0400

    Copied mario.py into search.py. Moved code to functions for searching for users and parsing the csv file. Currently the code prints the id of the first user search result for users with less than 5 results.

--- search.py
+++ search.py
@@ -3,19 +3,71 @@ import os
 import json
 from twitter import Api
 
+import urllib
+import itertools
+
 config = {}
 execfile("config.py", config)
-
-
 t = Api(config["consumer_key"], config["consumer_secret"], config["access_key"], config["access_secret"])
 
-results = t.GetSearch(
-            raw_query="q=engbert%20&result_type=recent&since=2014-07-19&count=10")
+num = 40 # max number of people to parse through
 
 
-results = t.GetUsersSearch("dan engbert", page=1, count=20, include_entities=None)
-for i in results:
-    print i
-    print ""
-    print ""
+def userSearch(term):
+    term = "'" + term + "'"
+    results = t.GetUsersSearch(term, page=1, count=20, include_entities=None)
 
+    if len(results) < 5 and len(results) > 0:
+        return results[0].id
+
+    return 0
+
+
+def readFromFile():
+    img = []
+    firstName = []
+    lastName = []
+    fullName = []
+    weight = []
+    rows = 0
+
+    f = open('FLORIDA.csv', 'rU' ) # open the file in read universal mode
+    for line in f:
+	    if rows == num+1:
+                break
+	    cells = line.split(",")
+	    fullName.append(cells[0].replace(' ', '') + " " + cells[2].replace(' ', ''))
+
+	    img.append(cells[13].replace(' ', '')) # since we want only 14th column
+	    firstName.append(cells[0].replace(' ', ''))
+	    lastName.append(cells[2].replace(' ', ''))
+	    weight.append(cells[8].replace(' ', ''))
+	    rows = rows + 1
+
+    f.close()
+    img.pop(0) # remove column title
+    firstName.pop(0) # remove column title
+    lastName.pop(0)
+    fullName.pop(0) # remove column title
+    weight.pop(0)
+
+    return fullName
+
+    firstName = map(str.lower, firstName) # makes the names uppercase
+    lastName = map(str.lower, lastName) # makes the names uppercase
+
+    for x in range(rows-1):
+	    print firstName[x] + " " + lastName[x]
+
+
+names = readFromFile()
+for n in names:
+    res = userSearch(n);
+    if res != 0:
+       print res
+       print ""
+       print ""
+
+
+    #print n + ", " + str(len(res))
+print "end"
