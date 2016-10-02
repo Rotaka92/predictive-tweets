5023d6c9025c03f759d29312b298dc4ef173c1c5
Daniel Engbert <end1@umbc.edu>
2016-10-02 02:07:21 -0400

    Made small changes to Mario's python script so it also saves last name.

--- mario.py
+++ mario.py
@@ -1,35 +1,32 @@
 import urllib
 import itertools
 
-output = []
-name = []
+img = []
+firstName = []
+lastName = []
 weight = []
 rows = 0
 
-f = open('FLORIDA.csv', 'rU' ) #open the file in read universal mode
+f = open('FLORIDA.csv', 'rU' ) # open the file in read universal mode
 for line in f:
-	if rows == 1000:
+	if rows == 10:
 		break
 	cells = line.split(",")
-	output.append(cells[13].replace(' ', '')) #since we want only 14th column
-	name.append(cells[0].replace(' ', ''))
+	img.append(cells[13].replace(' ', '')) # since we want only 14th column
+	firstName.append(cells[0].replace(' ', ''))
+	lastName.append(cells[2].replace(' ', ''))
 	weight.append(cells[8].replace(' ', ''))
-	print output[-1]
 	rows = rows + 1
-output.pop(0) #take out column title
-name.pop(0) #take out column title
+
+img.pop(0) # take out column title
+firstName.pop(0) # take out column title
+lastName.pop(0)
 weight.pop(0)
 f.close()
 
 
-name = map(str.upper,name) #makes the names uppercase
+firstName = map(str.lower, firstName) # makes the names uppercase
+lastName = map(str.lower, lastName) # makes the names uppercase
 
-print name
 for x in range(rows-1):
-	List = output[x]
-	Names = name[x]
-	Weight = weight[x]
-	print x
-	print "'" + List + "'"
-	if List != "" and List != " ":
-		urllib.urlretrieve(List, "pics/" + Names + "," + Weight + ".jpg")
+        print firstName[x] + " " + lastName[x]
