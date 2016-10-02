2678f8571ac2aec5cb4fcf2b7a7a02c4490fb23e
Daniel Engbert <end1@umbc.edu>
2016-10-01 23:43:34 -0400

    search.py now searches for a user based on a search term.

--- search.py
+++ search.py
@@ -2,18 +2,20 @@
 import os
 import json
 from twitter import Api
-#import oauth2 as oauth
-#from oauth2 import *
-
-import requests
-from requests import Session
 
 config = {}
 execfile("config.py", config)
 
-#session = Session()
 
 t = Api(config["consumer_key"], config["consumer_secret"], config["access_key"], config["access_secret"])
 
+results = t.GetSearch(
+            raw_query="q=engbert%20&result_type=recent&since=2014-07-19&count=10")
 
-print "hello"
+
+results = t.GetUsersSearch("dan engbert", page=1, count=20, include_entities=None)
+for i in results:
+    print i
+    print ""
+    print ""
+
